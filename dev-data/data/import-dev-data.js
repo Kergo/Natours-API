c54915fead568e929f36e21d6f83ff3ff4538129
Kergo <sskamenov@gmail.com>
2020-02-21 15:06:17 +0100

    Added API Filtering

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = `mongodb+srv://sergo:WZF2XfGuKcQotBy8@cluster0-hgbap.mongodb.net/natours?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// Read JSON File
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+//Import Data Into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// Delete All Data From DB
+const deleteDate = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteDate();
+}
+
+console.log(process.argv);
