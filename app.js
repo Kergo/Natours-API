b4010d692a3d3004e90a3616d3262dae28df3227
Kergo <sskamenov@gmail.com>
2020-02-21 01:27:26 +0100

    Added Controllers, Routes, Middleware, ESLint conf

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello from the server side!", app: "Natours" });
+// Middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from middle');
+  next();
 });
 
-app.post("/", (req, res) => {
-  res.send("You can suck it");
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+// Routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
