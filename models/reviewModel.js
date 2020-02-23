e5f36ff412767d67e451b32dacccf42ce6c072f6
Kergo <sskamenov@gmail.com>
2020-02-23 23:41:34 +0100

    Added Calculating Average Rating on Tours

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,6 @@
 // review / rating / createdAt
 const mongoogse = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoogse.Schema(
   {
@@ -48,6 +49,51 @@ reviewSchema.pre(/^find/, function(next) {
   });
 
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  // console.log(stats);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5
+    });
+  }
+};
+
+reviewSchema.post('save', function() {
+  // this points to current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+  //We store the id on the current query variable -> this.r
+  this.r = await this.findOne();
+  // console.log(this.r);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function() {
+  //this.r = await this.findOne(); <- DOES NOT WORK HERE, QUERY HAS ALREADY EXECUTED
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoogse.model('Review', reviewSchema);
