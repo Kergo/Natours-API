276247898e3b30980500fd19bde469ddd130506d
Kergo <sskamenov@gmail.com>
2020-02-23 18:07:55 +0100

    Added Virtual Populate: Tours and Reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -121,6 +121,13 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+// Virtual populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id'
+});
+
 // Middleware or also called Hooks
 
 // Document Middleware: runs before .save() and .create()
