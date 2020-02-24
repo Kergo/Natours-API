acaceee6d2a707f81b6c88b59bbf100276bbe7f7
Kergo <sskamenov@gmail.com>
2020-02-24 01:05:35 +0100

    Added Geospatial Aggregation: Calculating Distance

--- models/tourModel.js
+++ models/tourModel.js
@@ -179,16 +179,16 @@ tourSchema.pre(/^find/, function(next) {
 
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now - this.start} milliseconds!`);
-  console.log(docs);
+  // console.log(docs);
   next();
 });
 
 // Aggregation Middleware
-tourSchema.pre('aggregate', function(next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  // console.log(this.pipeline());
-  next();
-});
+// tourSchema.pre('aggregate', function(next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   // console.log(this.pipeline());
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
